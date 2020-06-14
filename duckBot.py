5e1b17681a1dfb032ee0aea963b89533d181cb9e
Laurie O <laurie_opperman@hotmail.com>
2020-06-14 18:47:49 +1000

    Magic 8

--- duckBot.py
+++ duckBot.py
@@ -82,5 +82,34 @@ async def send_goose_link(ctx, *, arg):
 
     await ctx.send(msg)
 
+@bot.command(name="magic8", help="Help me, I'm undecisive")
+async def magic8(ctx):
+
+    options = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes – definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful.",
+    ]
+
+    msg = random.choice(options)
+    await ctx.send(msg)
+
 bot.run(TOKEN)
 
